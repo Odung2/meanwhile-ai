48ab4546e12fe6355e51aeecf7bad62a96f9a456
su-giana <giananews@gmail.com>
2023-07-26 22:09:01 +0900

    26 Jul. (Sujee) : trend news part implementation done (90%)

--- meanwhile/urls.py
+++ meanwhile/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from articles.views import get_articles
+from rest_framework.routers import DefaultRouter
+from articles.views import ArticleViewSet, get_trend_news
+
+router = DefaultRouter()
+router.register(r'article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('article.urls'))
+    path('api/', include(router.urls)),
+    path('api/trend/', get_trend_news, name='get_trend_news')
 ]
